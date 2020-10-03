3746b1c369a9c83263a8449ef98b3e7acd8f4460
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2020-10-02 20:43:24 -0500

    does basic stuff w/ a list of host. Should work fine(tm) on one server but threads need to be added for more

--- mesher/go.py
+++ mesher/go.py
@@ -1,41 +1,23 @@
 #!#!/usr/bin/env python
 
 import json
-import socket
-import fcntl
-import struct
 
 from subprocess import Popen, PIPE
 
-hlist = ['idhavea.beer', 'google.com', 'myspace.com', 'idhavea.beer']
 host = 'idhavea.beer'
-# host = 'google.com'
+hlist = ['idhavea.beer', 'google.com', 'myspace.com', 'idhavea.beer']
 
-# command = f'ping -c 1 {host}'
-#
-# output = subprocess.call(command)
-# print(output)
-
-""" Find my own IP / dont work"""
-# def get_ip_address(ifname):
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     return socket.inet_ntoa(fcntl.ioctl(
-#         s.fileno(),
-#         0x8915,  # SIOCGIFADDR
-#         struct.pack('256s', ifname[:15])
-#     )[20:24])
-#
-# get_ip_address('en0')
 
 def ping(host):
-    command = ['ping', '-t', '3', '-c', '5', host]
+    command = ['ping', '-c', '5', host]
     p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
     rc = p.returncode
     o_dict = {}
-    o_dict['return_code'] = rc
-    o_dict['output'] = output
-    o_dict['error'] = err
+    o_dict['ping_rc'] = rc
+    o_dict['ping_output'] = output
+    o_dict['ping_error'] = err
+    o_dict['destination'] = host
     return o_dict
 
 def run_diag(host):
@@ -43,8 +25,6 @@ def run_diag(host):
     check_arp = ['arp', '-a']
     check_neigh = ['ip', 'neigh']
     check_host = ['ip', 'neigh', 'show', host]
-
-    # check_list = [check_arp, check_neigh, check_host]
 
     p = Popen(check_arp, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     o_dict['arp_table'] = p.communicate()[0]
@@ -55,36 +35,28 @@ def run_diag(host):
     p = Popen(check_host, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     o_dict['ip_neigh_explicit'] = p.communicate()[0]
 
-
     return o_dict
 
 
-for host in hlist:
-    o_dict = ping(host)
-    print(o_dict)
-    """ ping return codes? IE should be fine to match against 0
-    Success: code 0
-    No reply: code 1
-    Other errors: code 2 """
-
-    if o_dict['return_code'] == 0:
-        continue
-
-    else:
-        print(o_dict['output'])
-        run_diag(host)
-        print(json.dumps(o_dict, indent=4))
 
 
+def run():
+    for host in hlist:
+        o_dict = ping(host)
+        print(o_dict)
+        """ ping return codes? IE should be fine to match against 0
+        Success: code 0
+        No reply: code 1
+        Other errors: code 2 """
 
-#
-# def ping():
-#     try:
-#         output = subprocess.check_output(command, shell=True)
-#
-#     except:
-#         return False
-#
-#     return True
-#
-# print(ping())
+        if o_dict['ping_rc'] == 0:
+            msg = 'Successful 5 count ping to {}'
+            print(msg.format(host))
+            continue
+
+        else:
+            run_diag(host)
+            print(json.dumps(o_dict, indent=4))
+
+if __name__ == "__main__":
+    run()
