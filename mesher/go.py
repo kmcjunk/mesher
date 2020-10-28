68a457c6ece6766421ef8348c875b9f3960f1cdb
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2020-10-28 14:16:35 -0500

    multi proc works, its ghetto :shrug:

--- mesher/go.py
+++ mesher/go.py
@@ -2,36 +2,26 @@
 
 import json
 import logging
+import multiprocessing
+import os
+import signal
+import time
 
 from sys import argv
 from subprocess import Popen, PIPE
 from datetime import datetime
 
-# host = 'idhavea.beer'
-# hlist = ['idhavea.beer', 'google.com', 'myspace.com', 'idhavea.beer']
-
-# hlist = ['idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          'idhavea.beer',
-#          ]
 
 
-script, host = argv
+# script, host = argv
+
+hlist = ['ip',
+         'ip',
+         'ip',
+         'ip',
+         ]
+
+global pid
 
 def make_logger(app):
     logging.basicConfig(format='%(asctime)s - %(name)s - ' \
@@ -42,8 +32,8 @@ def make_logger(app):
                     #         logging.FileHandler(filename = app + '.log'),
                     #         logging.StreamHandler()
                     #         ],
-                    # filename= '/home/rackkevin/datadisk/mesher.log'
                     filename= 'mesher.log'
+                    # filename= 'mesher.log'
 
                     )
     logger = logging.getLogger(app)
@@ -64,19 +54,21 @@ def ping(host):
     o_dict['destination'] = host
     return o_dict
 
-def run_diag(host, o_dict):
+def check_pid(pid):
+    print('checkpid')
+    ps = 'ps awwffuxxx | grep {}'
+    return os.system(ps.format(pid))
+
+
+def run_diag(host, o_dict, tcp_pid=None):
     msg = '{} did not respond to pings, collectin data'
     logger.info(msg.format(host))
     dt_string = datetime.now().strftime("%H:%M:%S")
+    o_dict['event_time'] = dt_string
 
     check_arp = ['arp', '-a']
     check_neigh = ['ip', 'neigh']
     check_host = ['ip', 'neigh', 'show', host]
-    # location = '/home/rackkevin/datadisk/pcaps/{}_dump.pcap'
-    location = 'pcaps/{}_dump.pcap'
-
-    tcpdump = ['tcpdump', '-nni', 'eth1', '-c', '10000', '-s', '65535',
-               '-w', location.format(dt_string)]
 
     p = Popen(check_arp, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     o_dict['arp_table'] = p.communicate()[0]
@@ -87,10 +79,22 @@ def run_diag(host, o_dict):
     p = Popen(check_host, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     o_dict['ip_neigh_explicit'] = p.communicate()[0]
 
-    p = Popen(tcpdump, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    o_dict['capture_name'] = location.format(dt_string)
+    "I think try key error, if except - go? have a check on pid? but how"
+    "how to ger around an infintite loop on inserts? insert on run()"
+    "if not - insert pid. "
 
-    o_dict['event_time'] = dt_string
+    location = 'pcaps/{}_{}_dump.pcap'
+    tcpdump = ['tcpdump', '-nni', 'eth1', '-c', '10000', '-s', '65535',
+               '-w', location.format(host, dt_string)]
+    p = Popen(tcpdump, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    rc = p.returncode
+    time.sleep(90)
+    kill = 'kill {}'
+    os.system(kill.format(p.pid))
+
+    o_dict['tcp_pid'] = p.pid
+    o_dict['capture_name'] = location.format(host, dt_string)
 
     return o_dict
 
@@ -98,40 +102,49 @@ def run_diag(host, o_dict):
 
 
 def run(host):
-    hlist = []
-    hlist.append(host)
-    while True:
-        for host in hlist:
-            o_dict = ping(host)
-            """ ping return codes? IE should be fine to match against 0
-            Success: code 0
-            No reply: code 1
-            Other errors: code 2 """
+    o_dict = ping(host)
+    """ ping return codes? IE should be fine to match against 0
+    Success: code 0
+    No reply: code 1
+    Other errors: code 2 """
 
-            if o_dict['ping_rc'] == 0:
-                msg = 'Successful 5 count ping to {}'
-                logger.info(msg.format(host))
-                continue
+    if o_dict['ping_rc'] == 0:
+        msg = 'Successful 5 count ping to {}'
+        logger.info(msg.format(host))
+        pass
 
-            else:
-                run_diag(host, o_dict)
-                msg = 'event detected\n{}'
-                logger.error(msg.format(json.dumps(o_dict, indent=4)))
-                print(msg.format(json.dumps(o_dict, indent=4)))
-                "don't really need nice logging rn"
-                # for k,v in o_dict.items():
-                #     msg = ('{}:\n{}')
-                #
-                #     if not v:
-                #         v = 'Null'
-                #
-                #     if isinstance(v, str):
-                #         logger.error(msg.format(k,v.decode('utf-8')))
-                #
-                #     else:
-                #         logger.error(msg.format(k,v))
+    else:
+        run_diag(host, o_dict,)
+        msg = 'event detected\n{}'
+        logger.error(msg.format(json.dumps(o_dict, indent=4)))
+        print(msg.format(json.dumps(o_dict, indent=4)))
+        #     "don't really need nice logging rn"
+            # for k,v in o_dict.items():
+            #     msg = ('{}:\n{}')
+            #
+            #     if not v:
+            #         v = 'Null'
+            #
+            #     if isinstance(v, str):
+            #         logger.error(msg.format(k,v.decode('utf-8')))
+            #
+            #     else:
+            #         logger.error(msg.format(k,v))
 
 if __name__ == "__main__":
     logger = make_logger('mesher')
     print('Logging to ./mesher.log')
-    run(host)
+    pool = multiprocessing.Pool(processes=len(hlist))
+    try:
+        while True:
+            pool.map_async(run, hlist).get(9999999)
+    except KeyboardInterrupt:
+        pool.terminate()
+        print "killin tn lol"
+        exit(0)
+        pool.terminate()
+        pool.join()
+    except multiprocessing.TimeoutError as ex:
+            print 'timeout'
+
+    # run(host)
